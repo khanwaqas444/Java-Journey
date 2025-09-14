176a962d1a640470eb817511c7f8eb6517c1d757
khanwaqas444 <waqaskhan4447770@gmail.com>
2025-09-14 20:32:22 +0530

    Java-Journey

---
+++ src/conditionalStatement/Poly/Demo1.java
@@ -0,0 +1,16 @@
+package conditionalStatement.Poly;
+
+public class Demo1 {
+	
+	public static void main(String[] args) {
+		int a = 55;
+		int b = 33;
+		
+		if (a>b) {
+			System.out.println(6678);
+		} else {
+			System.out.println(8766);
+		}
+	}
+
+}
