50622941c791374000d0888beff1b91dae44daa7
khanwaqas444 <waqaskhan4447770@gmail.com>
2025-09-22 22:25:28 +0530

    Java-Journey

---
+++ src/conditionalStatement/Poly/If2.java
@@ -0,0 +1,13 @@
+package conditionalStatement.Poly;
+
+public class If2 {
+	
+	public static void main(String[] args) {
+		int a = 10;
+		
+		if (a>5) {
+			System.out.println("A is gretter");
+		}
+	}
+
+}
