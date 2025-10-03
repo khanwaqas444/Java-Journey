bc244a18e951cbaed9064000453f5d870398ac17
khanwaqas444 <waqaskhan4447770@gmail.com>
2025-10-03 21:31:58 +0530

    Java Journey

---
+++ src/conditionalStatement/Poly/If3.java
@@ -0,0 +1,13 @@
+package conditionalStatement.Poly;
+
+public class If3 {
+	
+	public static void main(String[] args) {
+		int a = 10;
+		
+		if(a>11) {
+			System.out.println("A is Gretter");
+		}
+	}
+
+}
