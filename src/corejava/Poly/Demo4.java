ed7e1cecddd417de80435402c5ee04f42ab24479
khanwaqas444 <waqaskhan4447770@gmail.com>
2025-10-09 20:55:07 +0530

    Java Journey

---
+++ src/corejava/Poly/Demo4.java
@@ -0,0 +1,12 @@
+package corejava.Poly;
+
+public class Demo4 {
+
+	public static void main(String[] args) {
+		String a = "waqas";
+		
+		if (a.equals("waqas")) {
+			System.out.println("Waqas is here");
+		}
+	}
+}
