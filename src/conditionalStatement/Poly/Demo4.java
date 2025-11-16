724df82c214cf41d04408fdca37e84f315fc033e
khanwaqas444 <waqaskhan4447770@gmail.com>
2025-11-16 23:34:05 +0530

    Java Journey

---
+++ src/conditionalStatement/Poly/Demo4.java
@@ -0,0 +1,16 @@
+package conditionalStatement.Poly;
+
+public class Demo4 {
+	
+	public static void main(String[] args) {
+		int a = 10;
+	    int b = 20;
+	    
+	    if (a>b) {
+	    	System.out.println("A is gretter");
+	    } else {
+	    	System.out.println("B is gretter");
+	    }
+	}
+
+}
