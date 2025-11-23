a99197e7de3bc79fa29ba2299e11e7200498a120
khanwaqas444 <waqaskhan4447770@gmail.com>
2025-11-23 20:12:14 +0530

    Java Journey

---
+++ src/conditionalStatement/Poly/Demo5.java
@@ -0,0 +1,15 @@
+package conditionalStatement.Poly;
+
+public class Demo5 {
+	
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 20;
+		
+		if(a>b) 
+		{
+			System.out.println("A is greater");
+		}
+	}
+
+}
