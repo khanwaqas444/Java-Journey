fd4d21084b8e5a7550020feaef727b794156275e
khanwaqas444 <waqaskhan4447770@gmail.com>
2025-11-27 09:37:31 +0530

    Java Codes

---
+++ src/corejava/Poly/Animal2.java
@@ -0,0 +1,16 @@
+package corejava.Poly;
+
+public class Animal2 {
+    public static void main(String[] args) {
+        // String (Immutable)
+        String str = "Hello";
+        str.concat(" World");
+        System.out.println("String: " + str);  // Output: Hello
+
+        // StringBuffer (Mutable)
+        StringBuffer sb = new StringBuffer("Hello");
+        sb.append("World");
+        System.out.println("StringBuffer: " + sb);  // Output: Hello World
+    }
+}
+
