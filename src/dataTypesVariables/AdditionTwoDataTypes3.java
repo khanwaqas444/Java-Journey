da1852bd734b62484574f01716438f422e5951b0
khanwaqas444 <waqaskhan4447770@gmail.com>
2025-11-29 21:15:16 +0530

    Java Codes

---
+++ src/dataTypesVariables/AdditionTwoDataTypes3.java
@@ -0,0 +1,13 @@
+package dataTypesVariables;
+
+public class AdditionTwoDataTypes3 {
+	
+	public static void main(String[] args) {
+		float a = 90.1f;
+		double b = 1.9;
+		double result = a + b;
+		
+		System.out.println("Result: " + result);
+	}
+
+}
