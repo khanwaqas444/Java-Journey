a44eba429cecf0deb9abdba38c116a3c7b8db5f3
khanwaqas444 <waqaskhan4447770@gmail.com>
2025-11-30 22:29:03 +0530

    Java Codes

---
+++ src/logicalcodes/EvenOdd.java
@@ -0,0 +1,14 @@
+package logicalcodes;
+
+public class EvenOdd {
+
+	public static void main(String[] args) {
+		int num = 44;
+		if (num % 2 == 0) {
+			System.out.println(num + " is Even");
+		} else {
+			System.out.println(num + " is Odd");
+		}
+		
+	}
+}
