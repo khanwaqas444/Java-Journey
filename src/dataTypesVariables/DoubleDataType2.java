9adc2904c07f47506809dbdee01af16531130e96
khanwaqas444 <waqaskhan4447770@gmail.com>
2025-12-05 20:57:00 +0530

    Java Codes

---
+++ src/dataTypesVariables/DoubleDataType2.java
@@ -0,0 +1,12 @@
+package dataTypesVariables;
+
+public class DoubleDataType2 {
+	
+	public static void main(String[] args) {
+		
+		double pi = 3.4987654;
+		
+		System.out.println("Value of PI: " + pi);
+	}
+
+}
