41c9c55a85e071e826300519eb5ff64882f0e751
khanwaqas444 <waqaskhan4447770@gmail.com>
2025-12-08 20:51:28 +0530

    Java Codes

---
+++ src/multithreading/DemoMulti2.java
@@ -0,0 +1,25 @@
+package multithreading;
+
+public class DemoMulti2 extends Thread 
+{
+	public void run() 
+	{
+	for(int i=1; i<=5; i++)
+	{
+		System.out.println("Waqas");
+ 	}
+}
+}
+class B 
+{
+	public static void main(String[] args) {
+		DemoMulti t=new DemoMulti();
+		t.start();
+		
+		for(int i=1; i<=5; i++)
+		{
+			System.out.println("Khan");
+		}
+	}
+
+}
