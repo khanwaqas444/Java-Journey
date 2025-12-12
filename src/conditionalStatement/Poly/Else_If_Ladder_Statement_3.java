588be111cea9f4d270e4258de6da651d211497bc
khanwaqas444 <waqaskhan4447770@gmail.com>
2025-12-12 23:22:47 +0530

    Java Codes

---
+++ src/conditionalStatement/Poly/Else_If_Ladder_Statement_3.java
@@ -0,0 +1,25 @@
+package conditionalStatement.Poly;
+
+public class Else_If_Ladder_Statement_3 {
+	
+	public static void main(String []args) {
+		int  marks = 59;
+		
+		if (marks >= 90) 
+		{
+			System.out.println("Grade: A");
+		}
+		else if(marks >= 60) 
+		{
+			System.out.println("Grade: B");
+		}
+		else if(marks >= 45) 
+		{
+			System.out.println("Grade: C");
+		}
+		else
+		{
+			System.out.println("Fail");
+		}
+	}
+}
