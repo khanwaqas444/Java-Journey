c2f818abed420a59ce28e4ecbea417d01421e522
khanwaqas444 <waqaskhan4447770@gmail.com>
2025-12-25 21:40:48 +0530

    Java Codes

---
+++ src/corejava/Poly/If_Statement.java
@@ -0,0 +1,12 @@
+package corejava.Poly;
+
+public class If_Statement {
+
+	public static void main(String[] args) {
+		String a = "waqas";
+		
+		if (a.equals("waqas")) {
+			System.out.println("Waqas is here");
+		}
+	}
+}
