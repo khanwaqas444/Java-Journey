6701be3d3fa72b558aeefef060ffb59fe58db3b3
khanwaqas444 <waqaskhan4447770@gmail.com>
2025-12-26 20:52:51 +0530

    Java Codes

---
+++ src/corejava/Poly/If_else.java
@@ -0,0 +1,14 @@
+package corejava.Poly;
+
+public class If_else {
+
+	public static void main(String[] args) {
+		int num = 44;
+		if (num % 2 == 0) {
+			System.out.println(num + " is Even");
+		} else {
+			System.out.println(num + " is Odd");
+		}
+		
+	}
+}
