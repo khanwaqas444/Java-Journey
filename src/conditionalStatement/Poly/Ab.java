b93cf28c7ddc196bf7b6644c8d22853e4e15a8bd
khanwaqas444 <waqaskhan4447770@gmail.com>
2026-01-03 22:39:51 +0530

    Java Codes

---
+++ src/conditionalStatement/Poly/Ab.java
@@ -0,0 +1,13 @@
+package conditionalStatement.Poly;
+
+public class Ab {
+	
+	public static void main(String[] args) {
+		int a = 10;
+		
+		if (a>5) {
+			System.out.println("A is gretter");
+		}
+	}
+
+}
